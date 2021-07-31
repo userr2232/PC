10a6b71024dbd615f2125f06d27dc177fe895345
userr2232 <reynaldo.rz.26@gmail.com>
2021-07-31 01:15:30 -0500

    articulation point and bridge

--- Study/articulation_point_and_bridge.cc
+++ Study/articulation_point_and_bridge.cc
@@ -1,37 +1,48 @@
 #include <iostream>
-#include <cstdio>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <pair>
+#include <cstdio>
+#define UNVISITED 0
 using namespace std;
+
 typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vii> AdjList;
 
-int dfsNumberCounter{0};
-vi dfs_num;
+AdjList graph;
+int dfsNumberCounter, dfsRoot, rootChildren;
+vi dfs_num, dfs_low, dfs_parent;
+vector<bool> articulation_vertex;
 
-void articulatioonPointAndBridge(int u) {
-    dfs_low[u] = dfs_num[u] = dfsNumberCounter++; // dfs_low[u] <= dfs_num[u]
-    for(int j = 0; j < AdjList[u].size(); ++j) {
-        auto [v, _] = AdjList[u][j];
+void articulationPointAndBridge(int u) {
+    dfs_num[u] = dfs_low[u] = dfsNumberCounter++;
+    for(int j = 0; j < graph[u].size(); ++j) {
+        auto [v, w] = graph[u][j];
         if(dfs_num[v] == UNVISITED) {
             dfs_parent[v] = u;
             if(u == dfsRoot) rootChildren++;
-            articulatioonPointAndBridge(v);
-            if(dfs_low[v] >= dfs_num[u]) articulation_vertex[u] = true;
-            if(dfs_low[v] > dfs_num[u]) printf(" Edge (%d, %d) is a bridge\n", u, v);
+            articulationPointAndBridge(v);
+            if(dfs_low[v] >= dfs_num[u]) articulation_vertex = true;
+            if(dfs_low[v] > dfs_num[u]) printf("Edge (%d, %d) is a bridge\n", u, v);
             dfs_low[u] = min(dfs_low[u], dfs_low[v]);
         }
-        else if(v != dfs_parent[u]) dfs_low[u] = min(dfs_low[u], dfs_num[v]);
+        else if(v != dfs_parent[u]) dfs_low[u] = min(dfs_low[u], dfs_num[v]); // BACKEDGE
     }
 }
 
 int main() {
+    dfsNumberCounter = 0;
     dfs_num.assign(V, UNVISITED);
     dfs_parent.assign(V, 0);
-    articulation_vertex.assign(V, 0);
-    printf("Bridges:\n");
+    articulation_vertex.assign(V, false);
+    cout << "Bridges" << endl;
     for(int i = 0; i < V; ++i) if(dfs_num[i] == UNVISITED) {
-        dfsRoot = i; rootChildren = 0;
-        articulatioonPointAndBridge(i);
-        articulation_vertex[dfsRoot] = rootChildren > 1;
+        dfsRoot = i, rootChildren = 0;
+        articulationPointAndBridge(i);
+        articulation_vertex[dfsRoot] = rootChildren > i;
     }
+    cout << "Articulation Points" << endl;
+    for(int i = 0; i < V; ++i) if(articulation_vertex[i]) cout << " Vertex" << i << endl;
 }
