e912411ebc91546fab3f8b7d1e563e32772a15eb
userr2232 <reynaldo.rz.26@gmail.com>
2021-08-02 01:06:25 -0500

    tarjan

--- Study/tarjan.cc
+++ Study/tarjan.cc
@@ -2,17 +2,17 @@
 #include <vector>
 #include <algorithm>
 #include <utility>
-#define FOR(i,a,b) for(int i = a; i <= b; ++i)
 #define UNVISITED 0
-#define VISITED 1
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
 using namespace std;
 typedef vector<int> vi;
 typedef pair<int,int> ii;
+typedef vector<ii> vii;
 typedef vector<vii> AdjList;
 AdjList graph;
-vi dfs_low, dfs_num, S;
+vi dfs_low, dfs_num;
 vector<bool> visited;
-int dfsNumberCounter, numSCC;
+int dfsNumberCounter{0}, numSCC{0};
 
 void tarjanSCC(int u) {
     dfs_low[u] = dfs_num[u] = dfsNumberCounter++;
@@ -24,10 +24,10 @@ void tarjanSCC(int u) {
         if(visited[v]) dfs_low[u] = min(dfs_low[u], dfs_low[v]);
     }
     if(dfs_low[u] == dfs_num[u]) {
-        printf("SCC %d:", ++numSCC);
+        cout << "SCC " << ++numSCC << endl;
         while(true) {
             int v = S.back(); S.pop_back(); visited[v] = false;
-            printf(" %d", v);
+            cout << " " << v;
             if(u == v) break;
         }
         cout << endl;
@@ -36,9 +36,7 @@ void tarjanSCC(int u) {
 
 int main() {
     int V, E; cin >> V >> E;
-    int u, v;
     graph.resize(V+1);
-    FOR(i,1,E) cin >> u >> v, graph[u].emplace_back(v), graph[v].emplace_back(u);
     dfs_num.assign(V+1, UNVISITED);
     dfs_low.assign(V+1, 0);
     visited.assign(V+1, false);
