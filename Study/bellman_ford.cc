f3b8d3b92f3d4baec18b6ba3032880856dd8d05d
userr2232 <reynaldo.rz.26@gmail.com>
2021-08-02 02:21:06 -0500

    bellman_ford

---
+++ Study/bellman_ford.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#define INF 2e9
+#define FOR(i,a,b) for(int i = a; i <= b; ++i)
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vii> AdjList;
+AdjList graph;
+vi dist;
+
+void bellmanFord(int s) {
+    dist[s] = 0;
+    FOR(i,1,V-1) FOR(u,1,V) FOR(j,0,graph[u].size()-1) {
+        auto [v, w] = graph[u][j];
+        dist[v] = min(dist[v], dist[u]+w);
+    }
+}
+
+int main() {
+    int V, E; cin >> V >> E;
+    graph.resize(V+1);
+    dist.assign(V+1, INF);
+    FOR(i,1,E) {
+        int u, v, w; cin >> u >> v >> w;
+        graph[u].emplace_back({v, w});
+        graph[v].emplace_back({u, w});
+    }
+    bellmanFord(1);
+}
