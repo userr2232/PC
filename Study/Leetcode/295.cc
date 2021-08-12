115e18ea3d8b87721c62f88cf84adc84fa310358
userr2232 <reynaldo.rz.26@gmail.com>
2021-08-12 10:13:38 -0500

    LeetCode/295.cc

---
+++ Study/Leetcode/295.cc
@@ -0,0 +1,37 @@
+class MedianFinder {
+    int n;
+    priority_queue<int> max_heap;
+    priority_queue<int, vector<int>, greater<int>> min_heap;
+public:
+    /** initialize your data structure here. */
+    MedianFinder(): n{0} {
+        
+    }
+    
+    void addNum(int num) {
+        if(n == 0) max_heap.push(num);
+        else{
+            min_heap.push(max_heap.top());
+            max_heap.pop();
+            min_heap.push(num);
+            max_heap.push(min_heap.top());
+            min_heap.pop();
+            if(max_heap.size() < min_heap.size()) {
+                max_heap.push(min_heap.top());
+                min_heap.pop();
+            }
+        }
+        n++;
+    }
+    
+    double findMedian() {
+        return n%2 ? max_heap.top() : 0.5*(max_heap.top()+min_heap.top());
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
