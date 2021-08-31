5b0e545dac9c5af36fb1d21895380ad0c968b88c
userr2232 <reynaldo.rz.26@gmail.com>
2021-08-31 15:56:24 -0500

    4.cc

--- LeetCode/4.cc
+++ LeetCode/4.cc
@@ -2,19 +2,33 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size(), n = nums2.size();
-        if(m > n) return findMedianSortedArrays(nums2, nums1);
-        int l = 0, r = m;
-        while(l <= r) {
-            int i = l + (r-l)/2;
-            int j = (m+n+1)/2-i;
-            if(i && nums1[i-1] > nums2[j]) r = i-1;
-            else if(i < m && nums2[j-1] > nums1[i]) l = i+1;
-            else {
-                int lmax = !i ? nums2[j-1] : (!j ? nums1[i-1] : max(nums1[i-1], nums2[j-1]));
-                if((m+n)%2) return lmax;
-                int rmin = i == m ? nums2[j] : (j == n ? nums1[i] : min(nums1[i], nums2[j]));
-                return (lmax+rmin)*0.5;
+        int l = (m+n+1) / 2;
+        int lo = max(0, l-n), hi = min(l, m);
+        while(lo <= hi) {
+            int x = (lo+hi)/2;
+            int y = l-x;
+            if(y < n && x > 0) {
+                if(nums1[x-1] > nums2[y]) {
+                    hi = x-1;
+                    continue;
+                } 
             }
+            if(x < m && y > 0) {
+                if(nums2[y-1] > nums1[x]) {
+                    lo = x+1;
+                    continue;
+                }
+            }
+            int lmax = x == 0 ? nums2[y-1] : y == 0 ? nums1[x-1] : max(nums1[x-1], nums2[y-1]);
+            if((m+n)%2) return lmax;
+            int rmin = x == m ? 
+                        y == n ?
+                            x == 0 ? nums2[y-1] : nums1[x-1] :
+                            nums2[y] :
+                        y == n ? 
+                            nums1[x] : 
+                            min(nums1[x], nums2[y]);
+            return 0.5*(lmax+rmin);
         }
         return 0;
     }
