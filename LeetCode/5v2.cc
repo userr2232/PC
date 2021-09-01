a0958304dbfaafc2d684d77087d7902f3d8175c7
userr2232 <reynaldo.rz.26@gmail.com>
2021-09-01 11:11:35 -0500

    5v2.cc

--- LeetCode/5v2.cc
+++ LeetCode/5v2.cc
@@ -1,23 +1,26 @@
 class Solution {
-public:
-    void expand(const string& s, int left, int right, int& longest, int& start) {
-        int n = s.length();
-        if(right == n || s[left] != s[right]) return;
-        while(right < n-1 && s[right] == s[right+1]) right++;
-        while(left > 0 && right < n-1 && s[left-1] == s[right+1]) left--, right++;
-        if(right-left+1 > longest) {
-            longest = right-left+1;
-            start = left;
+    int mx{0}, optim_i{0}, n;
+    string s;
+    
+    void expand(int i, int j) {
+        int len{0};
+        while(i >= 0 && j < n) {
+            if(s[i] == s[j]) {
+                len = j-i+1;
+                if(len > mx) mx = len, optim_i = i;
+                --i, ++j;
+            }
+            else break;
         }
     }
     
+public:
     string longestPalindrome(string s) {
-        int n = s.length();
-        int longest{0}, start{0};
+        n = s.length(), this->s = s;
         for(int i = 0; i < n; ++i) {
-            expand(s,i,i,longest,start);
-            expand(s,i,i+1,longest,start);
+            expand(i, i);
+            if(i) expand(i-1, i);
         }
-        return s.substr(start, longest);
+        return s.substr(optim_i, mx);
     }
 };
