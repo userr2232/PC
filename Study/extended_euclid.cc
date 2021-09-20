dd99276574643ff9887c3334c9e12655d78879e8
userr2232 <reynaldo.rz.26@gmail.com>
2021-09-20 08:54:01 -0400

    extended_euclid explanation

--- Study/extended_euclid.cc
+++ Study/extended_euclid.cc
@@ -1,17 +1,23 @@
 #include <iostream>
-#include <utility>
+#include <tuple>
+
 using namespace std;
+// ax + by = gcd(a, b) = gcd(b, a%b)
+// bx' + (a%b)y' = gcd(b, a%b)
+// bx' + (a-a/b*b)y' = gcd(b, a%b)
+// ay' + b(x'-a/by') = gcd(b, a%b) = gcd(a, b)
 
 tuple<int,int,int> gcd(int a, int b) {
     if(b == 0) return {1, 0, a};
     int x, y, g;
     tie(x, y, g) = gcd(b, a%b);
-    return {y, x - a/b * y, g};
+    return {y, x-a/b*y, g};
 }
 
 int main() {
     int x, y, g;
-    tie(x, y, g) = gcd(30, 12);
-    cout << x << " " << y << " " << g << endl;
+    // 12x + 30y = 6
+    tie(x, y, g) = gcd(12, 30);
+    cout << "x: " << x << " y: " << y << " gcd(12, 30): " << g << endl;
     return 0;
 }
