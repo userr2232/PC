890ca980ac8b06862d4c839fbbc76df7b09be421
userr2232 <reynaldo.rz.26@gmail.com>
2021-10-07 14:15:23 -0500

    mod next_permutation

--- Study/next_permutation.cc
+++ Study/next_permutation.cc
@@ -1,16 +1,20 @@
 #include <iostream>
-#include <string>
+#include <vector>
 #include <algorithm>
+#include <iterator>
 using namespace std;
+typedef vector<int> vi;
 
-void next_permutation(string& s) {
-    auto it = is_sorted_until(rbegin(s), rend(s));
-    if(it != rend(s)) swap(*it, *upper_bound(rbegin(s), it, *it));
-    reverse(rbegin(s), it);
+void next_permutation(vi& v) {
+    auto it = is_sorted_until(rbegin(v), rend(v));
+    if(it != rend(v)) swap(*it, *upper_bound(rbegin(v), it, *it));
+    reverse(rbegin(v), it);
 }
 
 int main() {
-    string s; cin >> s;
-    next_permutation(s);
-    cout << "next_permutation: " << s << endl;
+    int n; cin >> n;
+    vi v(n); for(auto & e: v) cin >> e;
+    next_permutation(v);
+    for(auto& e : v) cout << e << " ";
+    return 0;
 }
