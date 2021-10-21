7c91049a1cbde321a13e4e9e90fde449b27fd6a5
userr2232 <reynaldo.rz.26@gmail.com>
2021-10-21 11:28:03 -0500

    comment

--- Study/sparse_table.cc
+++ Study/sparse_table.cc
@@ -12,7 +12,7 @@ public:
     SparseTable(vi& initialA) {
         A = initialA;
         N = A.size();
-        LOG = 31-__builtin_clz(N)+1;
+        LOG = 31-__builtin_clz(N)+1; // +1 in case N is a power of 2 and we need to have k == LOG in RMQ
         SpT.resize(N, vector<int>(LOG, 0));
         for(int i = 0; i < N; ++i) SpT[i][0] = A[i];
         for(int j = 1; j < LOG; ++j) for(int i = 0; i + (1<<j) - 1 < N; ++i) {
@@ -26,3 +26,10 @@ public:
         return min(SpT[L][k], SpT[R-(1<<k)+1][k]);
     }
 };
+
+int main() {
+    int n; cin >> n;
+    vi A(n); for(auto& e : A) cin >> e;
+    auto spt = SparseTable(A);
+    cout << spt.RMQ(0, 7) << endl;
+}
