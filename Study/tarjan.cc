937085add99c42f5c99f024eb7a7eeb222a79bbb
userr2232 <reynaldo.rz.26@gmail.com>
2021-10-26 00:56:40 -0500

    fixed tarjan

--- Study/tarjan.cc
+++ Study/tarjan.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <stack>
 #include <utility>
 #define UNVISITED -1
 #define FOR(i,a,b) for(int i = a; i <= b; ++i)
@@ -10,42 +10,42 @@ typedef pair<int,int> ii;
 typedef vector<ii> vii;
 typedef vector<vii> AdjList;
 
-AdjList graph;
-vi dfs_low, dfs_num, S;
-vector<bool> visited;
-
 int dfsNumberCounter, SCC;
+vector<bool> visited;
+stack<int> st;
+vi dfs_num, dfs_low;
+AdjList graph;
 
 void tarjanSCC(int u) {
-    dfs_low[u] = dfs_num[u] = ++dfsNumberCounter;
+    dfs_num[u] = dfs_low[u] = ++dfsNumberCounter;
     visited[u] = true;
-    S.emplace_back(u);
+    st.emplace(u);
     FOR(j,0,graph[u].size()-1) {
         auto [v, _] = graph[u][j];
         if(dfs_num[v] == UNVISITED) tarjanSCC(v);
         if(visited[v]) dfs_low[u] = min(dfs_low[u], dfs_low[v]);
-        if(dfs_num[u] == dfs_low[u]) {
-            cout << "SCC: " << ++SCC << endl;
-            while(true) {
-                int v = S.back();
-                S.pop_back();
-                visited[v] = false;
-                if(u == v) break;
-            }
+    }
+    if(dfs_low[u] == dfs_num[u]) {
+        cout << "SCC: " << ++SCC << endl;
+        while(true) {
+            int v = st.top();
+            st.pop();
+            cout << v << " ";
+            visited[v] = false;
+            if(v == u) break;
         }
     }
 }
 
 int main() {
     int V, E; cin >> V >> E;
-    graph.resize(V);
-    dfs_low.resize(V);
-    dfs_num.assign(V, UNVISITED);
     visited.assign(V, false);
+    dfs_num.assign(V, UNVISITED);
+    dfs_low.assign(V, -1);
+    graph.resize(V);
     FOR(i,1,E) {
         int u, v, w; cin >> u >> v >> w;
         graph[u].emplace_back(v, w);
-        graph[v].emplace_back(u, w);
     }
     FOR(u,0,V-1) if(dfs_num[u] == UNVISITED) tarjanSCC(u);
 }
