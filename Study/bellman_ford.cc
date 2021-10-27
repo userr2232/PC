b239122274f93dd4bfa2f82aaf93e8d43171b352
userr2232 <reynaldo.rz.26@gmail.com>
2021-10-26 20:58:25 -0500

    0 indexed graph

--- Study/bellman_ford.cc
+++ Study/bellman_ford.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <utility>
 #define INF 2e9
 #define FOR(i,a,b) for(int i = a; i <= b; ++i)
@@ -9,31 +9,31 @@ typedef vector<int> vi;
 typedef pair<int,int> ii;
 typedef vector<ii> vii;
 typedef vector<vii> AdjList;
-AdjList graph;
+
 vi dist;
+AdjList graph;
 
 void bellmanFord(int s) {
     dist[s] = 0;
-    FOR(i,1,V-1) FOR(u,1,V) FOR(j,0,graph[u].size()-1) {
+    FOR(i,1,V-1) FOR(u,0,V-1) FOR(j,0,graph[u].size()-1) {
         auto [v, w] = graph[u][j];
         dist[v] = min(dist[v], dist[u]+w);
     }
-    bool hasNegativeCycle = false;
-    FOR(u,1,V) FOR(j,0,graph[u].size()-1) {
+    bool hasNegativeWeightCycle{false};
+    FOR(u,0,V-1) FOR(j,0,graph[u].size()-1) {
         auto [v, w] = graph[u][j];
-        if(dist[v] > dist[u] + w) hasNegativeCycle = true;
+        if(dist[v] < dist[u]+w) hasNegativeWeightCycle = true;
     }
-    if(hasNegativeCycle) cout << "HAS NEGATIVE CYCLE!!!!" << endl;
+    if(hasNegativeWeightCycle) cout << "HAS NEGATIVE WEIGHT CYCLE!!!!" << endl;
 }
 
 int main() {
-    int V, E; cin >> V >> E;
-    graph.resize(V+1);
-    dist.assign(V+1, INF);
+    cin >> V >> E;
+    dist.assign(V, INF);
+    graph.resize(V);
     FOR(i,1,E) {
         int u, v, w; cin >> u >> v >> w;
-        graph[u].emplace_back({v, w});
-        graph[v].emplace_back({u, w});
+        graph[u].emplace_back(v, w);
     }
-    bellmanFord(1);
+    bellmanFord(0);
 }
