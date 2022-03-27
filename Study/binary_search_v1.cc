969f841f59529c8cb0576c8276a22f87a66ec463
userr2232 <reynaldo.rz.26@gmail.com>
2022-03-26 21:04:25 -0500

    fix: binary

--- Study/binary_search_v1.cc
+++ Study/binary_search_v1.cc
@@ -1,21 +1,19 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
-typedef vector<int> vi;
+
 int main() {
     int n, x; cin >> n >> x;
-    vi v(n);
-    for(auto&e : v) cin >> e;
+    vector<int> v(n);
+    for(auto& e : v) cin >> e;
     int a{0}, b{n-1};
     while(a <= b) {
-        int k = (a + b) / 2;
-        if(v[k] == x) {
+        int m = a + (b-a)/2;
+        if(v[m] == x) {
             // found
-            return 0;
         }
-        if(v[k] < x) a = k-1;
-        else b = k+1;
+        else if(v[m] < x) a = m+1;
+        else b = m-1;
     }
-    return 0;
 }
