75cf10fb8c12b9f6cbca81b186f4899865aff01b
userr2232 <reynaldo.rz.26@gmail.com>
2022-04-07 20:58:19 -0500

    LeetCode/206v2.cc

---
+++ LeetCode/206v2.cc
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == nullptr) return nullptr;
+        ListNode *prev{nullptr}, *current{head}, *next{head->next};
+        while(current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+};
