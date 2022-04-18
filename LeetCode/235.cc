b98cb77df542709ef3e8b414583cbdb11e19d729
userr2232 <reynaldo.rz.26@gmail.com>
2022-04-18 16:45:54 -0500

    LeetCode/235.cc

---
+++ LeetCode/235.cc
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr) return nullptr;
+        if(root == p || root == q) return root;
+        auto&& left = lowestCommonAncestor(root->left, p, q);
+        auto&& right = lowestCommonAncestor(root->right, p, q);
+        if(left && right) return root;
+        if(left) return left;
+        if(right) return right;
+        return nullptr;
+    }
+};
