8beb1dd8966ebed90f7360c73b3b672c672dfd95
userr2232 <reynaldo.rz.26@gmail.com>
2022-04-18 16:36:04 -0500

    LeetCode/230.cc

---
+++ LeetCode/230.cc
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int ans;
+    
+    void dfs_helper(TreeNode* root, int& k) {
+        if(root == nullptr) return;
+        dfs_helper(root->left, k);
+        if(--k == 0) ans = root->val;
+        dfs_helper(root->right, k);
+    }
+    
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        dfs_helper(root, k);
+        return ans;
+    }
+};
