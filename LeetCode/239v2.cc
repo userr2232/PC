310774e53b8b36a9cd54d725b4f6603d23c049a0
userr2232 <reynaldo.rz.26@gmail.com>
2022-05-10 19:11:10 -0500

    LeetCode/239.cc

---
+++ LeetCode/239v2.cc
@@ -0,0 +1,18 @@
+class Solution {    
+    void clear(deque<int>& dq, int i, vector<int> const& nums, const int k) {
+        if(!dq.empty() && dq.front() <= i-k) dq.pop_front();
+        while(!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
+    }
+    
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        const int n = nums.size();
+        for(int i = 0; i < n; ++i) {
+            clear(dq, i, nums, k), dq.emplace_back(i);
+            if(i >= k-1) ans.emplace_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
